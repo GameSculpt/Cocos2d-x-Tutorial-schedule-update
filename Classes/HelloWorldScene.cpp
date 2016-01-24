3f4af5ed42dd9581659df4c5b5217f0ec2e21567
erfanumar <erfanspecialist@gmail.com>
2016-01-24 15:44:35 +0800

    commit message

---
+++ Classes/HelloWorldScene.cpp
@@ -0,0 +1,86 @@
+#include "HelloWorldScene.h"
+
+USING_NS_CC;
+
+using namespace ui;
+
+float goUp = 0;
+
+Scene* HelloWorld::createScene()
+{
+    // 'scene' is an autorelease object
+    auto scene = Scene::create();
+    
+    // 'layer' is an autorelease object
+    auto layer = HelloWorld::create();
+
+    // add layer as a child to scene
+    scene->addChild(layer);
+
+    // return the scene
+    return scene;
+}
+
+// on "init" you need to initialize your instance
+bool HelloWorld::init()
+{
+    //////////////////////////////
+    // 1. super init first
+    if ( !Layer::init() )
+    {
+        return false;
+    }
+
+	auto winSize = Director::getInstance()->getWinSize();
+
+	spriteName = Sprite::create("girl.png");
+	spriteName->setPosition(Vec2(winSize.width/2,winSize.height/2));
+	this->addChild(spriteName);
+
+
+	auto button = Button::create("Button.png", "ButtonPressed.png", "ButtonPressed.png");
+	button->setTitleText("Spawn Girl");
+	button->setTitleFontName("MarkerFelt.ttf");
+	button->setTitleFontSize(12.0f);
+	button->setPosition(Vec2(winSize.width/2, winSize.height/2 - 100));
+
+	button->addTouchEventListener([&](Ref* sender, Widget::TouchEventType type)
+	{
+		switch (type)
+		{
+		case cocos2d::ui::Widget::TouchEventType::BEGAN:
+			break;
+		case cocos2d::ui::Widget::TouchEventType::MOVED:
+			break;
+		case cocos2d::ui::Widget::TouchEventType::ENDED:		
+			break;
+		case cocos2d::ui::Widget::TouchEventType::CANCELED:
+			break;
+		default:
+			break;
+		}
+	});
+
+	this->addChild(button);
+
+	scheduleUpdate();
+
+    return true;
+}
+
+
+void HelloWorld::menuCloseCallback(Ref* pSender)
+{
+    Director::getInstance()->end();
+
+#if (CC_TARGET_PLATFORM == CC_PLATFORM_IOS)
+    exit(0);
+#endif
+}
+
+void HelloWorld::update(float dt)
+{
+	goUp++;
+	auto winSize = Director::getInstance()->getWinSize();
+	spriteName->setPosition(Vec2(winSize.width/2,winSize.height/2 + goUp));
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+Cocos2d-x Tutorial http://gamesculpt.blogspot.my/<br />
+Schedule update (part 1) http://gamesculpt.blogspot.com/2016/01/cocos2d-x-c-schedule-update.html<br />
+Video https://youtu.be/Pm_d_YjGak4
